29bee3b0261def3f56cc410b03ac76d360952294
woxihejinghao <719549112@qq.com>
2017-06-15 19:08:04 +0800

    test

---
+++ JHRecruit/JHRecruit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JHRecruit
+//
+//  Created by milan on 2017/6/15.
+//  Copyright © 2017年 milan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JHRecruit/JHRecruit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JHRecruit
+//
+//  Created by milan on 2017/6/15.
+//  Copyright © 2017年 milan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
